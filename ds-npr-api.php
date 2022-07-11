d11ae6aa1be0021216041d38723a4318d0b8b7ff
William Tam <tamw@wnet.org>
2022-07-11 14:55:18 -0400

    v1.9.4.1

--- ds-npr-api.php
+++ ds-npr-api.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: NPR Story API
  * Description: A collection of tools for reusing content from NPR.org, now maintained and updated by NPR member station developers
- * Version: 1.9.4
+ * Version: 1.9.4.1
  * Author: Open Public Media
  * License: GPLv2
 */
